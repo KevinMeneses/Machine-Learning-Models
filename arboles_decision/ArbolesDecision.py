13b02b7655acfd48a0ffdbe176c7c2732a7054a6
Kevin Meneses <kevmeneses@gmail.com>
2020-04-12 20:18:36 -0500

    ignore files.

---
+++ arboles_decision/ArbolesDecision.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import confusion_matrix
+
+# load dataset from csv file
+data = pd.read_csv("D:/Proyecto/datosfinal.csv")
+
+# One-hot encode the data using pandas get_dummies
+data = pd.get_dummies(data)
+# set the dependent variable
+labels = np.array(data['Actividad'])
+# Remove the labels from the features
+data = data.drop('Actividad', axis=1)
+# Saving feature names for later use
+data_list = list(data.columns)
+# Convert to numpy array
+data = np.array(data)
+
+# split the data
+x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.25)
+
+model = DecisionTreeClassifier(random_state=1)
+
+# set the number of folds
+folds = range(1, 10)
+
+# train the model with folds
+for j in folds:
+    print('\nFold ', j)
+    # split the data
+    x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.25)
+    # fit model
+    model.fit(x_train, y_train)
+    # test the model
+    y_pred = model.predict(x_test)
+    # Calculate the absolute errors
+    print(f'Model Accuracy: {model.score(x_test, y_test)}')
+
+y_pred = model.predict(x_test)
+# Calculate the absolute errors
+print(f'Model Accuracy: {model.score(x_test, y_test)}')
+# create the confusion matrix
+print(confusion_matrix(y_true=y_test, y_pred=y_pred))
